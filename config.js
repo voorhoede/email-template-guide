d6861b4ba1ef274712438b7baea43f375aa30a03
Vincent van Dijck <vvandijck@MacBook-Pro-van-Vincent-3.local>
2016-07-04 11:54:52 +0200

    Updated project features.
    Checked and updated current features.
    Added tasks for keeping any media queries present in the project.

--- config.js
+++ config.js
@@ -1,81 +1,100 @@
+'use strict';
+
+/**
+ * Paths configuration.
+ */
 var paths = {
-	src: 'src/',
-	srcComponents: 'src/components/',
-	srcVendor: 'src/vendor/',
-	srcViews: 'src/views/',
-	dist: 'dist/',
-	distAssets: 'dist/assets/',
-	distComponents: 'dist/components/',
-	distViews: 'dist/views/',
-	karmaConfig:'./test/karma.conf.js',
-	changelog:'CHANGELOG.md'
+    src: 'src/',
+    srcComponents: 'src/components/',
+    srcVendor: 'src/vendor/',
+    srcViews: 'src/views/',
+    dist: 'dist/',
+    distAssets: 'dist/assets/',
+    distComponents: 'dist/components/',
+    distViews: 'dist/views/'
 };
 
 paths.assetFiles = [
-		paths.src + 'assets/**/*.*',
-		paths.srcComponents + '*/assets/**/*.*',
-		paths.srcViews + '*/assets/**/*.*',
-		paths.templates
+    paths.src + 'assets/**/*.*',
+    paths.srcComponents + '*/assets/**/*.*',
+    paths.srcViews + '*/assets/**/*.*'
 ];
 
 // source files are all files directly in module sub directories and core files,
 // excluding abstract files/dirs starting with '_'.
 paths.srcFiles = [
-		paths.src + '*',
-		paths.srcComponents + '*/*',
-		paths.srcViews + '*/*',
-		'!' + paths.src + '*/_template/*'
+    paths.src + '*',
+    paths.srcComponents + '*/*',
+    paths.srcViews + '*/*',
+    '!' + paths.src + '*/_template/*'
 ];
 
-paths.htmlFiles = paths.srcFiles.map(function(path){ return path + '.html'; });
-paths.scssFiles = paths.srcFiles.map(function(path){ return path + '*/*.scss'; });
+paths.htmlFiles = paths.srcFiles.map(function (path) {
+    return path + '.html';
+});
 
-var litmusSelectedApplications = {
-		'ol2003' : true,
-		'ol2007' : true,
-		'ol2010' : true,
-		'ol2013' : true,
-		'appmail6' : true,
-		'outlookcom' : true,
-		'yahoo' : true,
-		'gmailnew' : true,
-		'ffgmailnew' : true,
-		'chromegmailnew' : true,
-		'android4' : true,
-		'androidgmailapp' : true,
-		'ipad' : true,
-		'ipadmini' :  true,
-		'iphone5s' : true,
-		'windowsphone8' : true
+paths.scssFiles = paths.srcFiles.map(function (path) {
+    return path + '*/*.scss';
+});
+
+/**
+ * Browserify configuration.
+ * @type {{server: {baseDir: string}, ui: boolean, online: boolean, open: boolean}}
+ */
+var browserDefaultConfig = {
+    files: [
+        'dist/assets/index.css',
+        'dist/**/*.html',
+        'dist/assets/*.js'
+    ],
+    logFileChanges: false,
+    notify: false,
+    online: false,
+    open: false,
+    port: 3000,
+    server: {
+        baseDir: paths.dist
+    },
+    ui: false
 };
 
-var litmusUserCredentials = {
-	username: 'litmus_username',
-	password: 'litmus_password',
-	url: 'https://yourcompany.litmus.com'
+/**
+ * Litmus configuration.
+ *
+ * To get the codes:
+ * https://yourcompany.litmus.com/emails/clients.xml
+ *
+ * NOTE: Make sure that the number of clients in 'applications' is the same as in your
+ * Litmus settings.
+ */
+var litmusConfig = {
+    username: 'litmus_username',
+    password: 'litmus_password',
+    url: 'https://yourcompany.litmus.com',
+    applications: [
+        'ol2000',
+        'ol2002',
+        'ol2003',
+        'ol2007',
+        'ol2010',
+        'ol2011',
+        'ol2013',
+        'ol2015',
+        'ol2016'
+    ]
 };
-
-var applications = [];
-
-for(apps in litmusSelectedApplications){
-	if(litmusSelectedApplications[apps] === true) {
-		applications.push(apps);
-	}
-}
-
-var litmusConfig = litmusUserCredentials;
-litmusConfig.applications = applications;
 
 var emailSMTPCredentials = {
-	service: 'Gmail',
-	auth: {
-		user: 'email.template.guide@gmail.com',
-		pass: ''
-	}
+    service: 'Gmail',
+    auth: {
+        user: 'youremail@gmail.com',
+        pass: 'password'
+    }
 };
 
 module.exports = {
-	paths: paths,
-	litmusConfig : litmusConfig,
-	emailSMTPCredentials : emailSMTPCredentials
+    paths: paths,
+    litmusConfig: litmusConfig,
+    emailSMTPCredentials: emailSMTPCredentials,
+    browserSync: browserDefaultConfig
 };
